ff02a74b0d2f291d36093de8b03be9e13f78acb1
VictorKaue <victorkaue909@gmail.com>
2024-09-16 08:51:19 -0300

    correcao

--- gtr_bluetooth_controlled.cpp
+++ gtr_bluetooth_controlled.cpp
@@ -36,42 +36,42 @@ void loop(){
   delay(200);
 
   while(Serial.available()){
-    INBYTE = Serial.read();        
+    INBYTE = Serial.read();
     if(distance <= 100 || digitalRead(IR1) == HIGH || digitalRead(IR2) == HIGH){
       digitalWrite(motor_acelerar, LOW);
       digitalWrite(motor_ré, LOW)
     } else {
       if(INBYTE == 'W' || INBYTE == 'w'){
-        digitalWrite(motor_acelerar, HIGH);
-        delay(600);
-        digitalWrite(motor_acelerar, LOW);
+      digitalWrite(motor_acelerar, HIGH);
+      delay(600);
+      digitalWrite(motor_acelerar, LOW);
       }
-      if(INBYTE == 'S' || INBYTE == 's'){
-        digitalWrite(motor_ré, HIGH);
-        delay(600);
-        digitalWrite(motor_ré, LOW);
+    }
+    if(INBYTE == 'S' || INBYTE == 's'){
+      digitalWrite(motor_ré, HIGH);
+      delay(600);
+      digitalWrite(motor_ré, LOW);
+    }
+    if(INBYTE == 'A' || INBYTE == 'a'){
+      for(i; i < 180; i++){
+        servo.write(i);
+        delay(8);
       }
-      if(INBYTE == 'A' || INBYTE == 'a'){
-        for(i; i < 180; i++){
-          servo.write(i);
-          delay(8);
-        }
+    }
+    if(INBYTE == 'D' || INBYTE == 'd'){
+      for(i; i > 0; i--){
+        servo.write(i);
+        delay(8);
       }
-      if(INBYTE == 'D' || INBYTE == 'd'){
-        for(i; i > 0; i--){
-          servo.write(i);
-          delay(8);
-        }
+    }
+    if(INBYTE == 'X' || INBYTE == 'x'){
+      for(i; i<90; i++){
+        servo.write(i);
+        delay(8);
       }
-      if(INBYTE == 'X' || INBYTE == 'x'){
-        for(i; i<90; i++){
-          servo.write(i);
-          delay(8);
-        }
-        for(i; i>90; i--){
-          servo.write(i);
-          delay(8);
-        }
+      for(i; i>90; i--){
+        servo.write(i);
+        delay(8);
       }
     }
   }
