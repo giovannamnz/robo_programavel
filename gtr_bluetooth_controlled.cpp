39474b1f3a9deb7ccc1cdeb7a659c36a323a361e
VictorKaue <98410524+VictorKaue@users.noreply.github.com>
2024-09-23 17:36:57 -0300

    Mustang com bluetooth arduino

--- gtr_bluetooth_controlled.cpp
+++ gtr_bluetooth_controlled.cpp
@@ -1,109 +1,53 @@
-#include <ESP32Servo.h>
-#include <WiFi.h>
-
-const char* ssid = "optimusXP";
-const char* password = "robocrazy";
-WiFiServer server(80);
-
+#include <Servo.h>
 Servo servo;
 int i = 90;
-const int motor_frente = 4;
-const int motor_re = 2;
-const int trigPin = 22;
-const int echoPin = 18;
-const int servoPin = 33;
+char INBYTE;
+const int motor_frente = 2;
+const int motor_re = 4;
+const int infra_vermelho = 10;
+const int trigPin = 13;
+const int echoPin = 12;
 long unsigned duration;
 float distance;
-const float distanciaSegura = 20.0; // Definir a distância mínima segura (em cm)
-const int infra_vermelho1 = 34;
+const float distanciaSegura = 20.0;
 
 void setup() {
-  Serial.begin(115200); // Inicialize a comunicação serial
-  Serial.println();
-  Serial.print("Conectando-se a ");
-  Serial.println(ssid);
-
-  WiFi.begin(ssid, password);
-
-  ESP32PWM::allocateTimer(0);
-  ESP32PWM::allocateTimer(1);
-  ESP32PWM::allocateTimer(2);
-  ESP32PWM::allocateTimer(3);
-  servo.setPeriodHertz(50);
-  servo.attach(servoPin, 500, 2400);
-
+  servo.attach(8);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
+  pinMode(infra_vermelho, INPUT);
   pinMode(motor_frente, OUTPUT);
   pinMode(motor_re, OUTPUT);
-  pinMode(infra_vermelho1, INPUT);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-
-  Serial.println("");
-  Serial.println("WiFi conectada.");
-  Serial.println("Endereço de IP: ");
-  Serial.println(WiFi.localIP());
-
-  server.begin();
+  Serial.begin(9600);
 }
 
-void loop() {
-  WiFiClient client = server.available(); // Aguarda um cliente conectar
-  if (client) {
-    Serial.println("Novo cliente conectado.");
-    String currentLine = "";
-    
-    while (client.connected()) {
-      if (client.available()) {
-        char c = client.read();
-        Serial.write(c); // Mostra o caractere recebido no monitor serial
-
-        if (c == '\n') {
-          // Se a linha estiver vazia, envia uma resposta HTML
-          if (currentLine.length() == 0) {
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println();
-            client.println("Click <a href='/W'>aqui</a> para ir para frente");
-            client.println("Click <a href='/S'>aqui</a> para ir para trás");
-            client.println("Click <a href='/A'>aqui</a> para virar à esquerda");
-            client.println("Click <a href='/D'>aqui</a> para virar à direita");
-            client.println("Click <a href='/X'>aqui</a> para ajustar as rodas na posição neutra");
-            client.println();
-            break;
-          } else {
-            currentLine = "";
-          }
-        } else if (c != '\r') {
-          currentLine += c;
-        }
-
-        // Verifica os comandos recebidos pela URL
-        if (currentLine.endsWith("GET /W")) {
-          moverParaFrente();
-        }
-        if (currentLine.endsWith("GET /S")) {
-          moverParaTras();
-        }
-        if (currentLine.endsWith("GET /A")) {
-          virarEsquerda();
-        }
-        if (currentLine.endsWith("GET /D")) {
-          virarDireita();
-        }
-        if (currentLine.endsWith("GET /X")) {
-          ajustarPosicaoNeutra();
-        }
-      }
-      verificarDistancia();
+void loop(){
+  verificarDistancia();
+  while(Serial.available()){
+    INBYTE = Serial.read();
+    if(INBYTE == 'W' || INBYTE == 'w'){
+      Serial.println("Movendo para frente");
+      moverParaFrente();
     }
-    client.stop();
-    Serial.println("Cliente desconectado.");
+    if(INBYTE == 'S' || INBYTE == 's'){
+      Serial.println("Movendo para tras");
+       moverParaTras();
+    }
+    if(INBYTE == 'A' || INBYTE == 'a'){
+      Serial.println("Movendo para esquerda");
+      virarEsquerda();
+    }
+    if(INBYTE == 'D' || INBYTE == 'd'){
+      Serial.println("Movendo para direita");
+      virarDireita();
+    }
+    if(INBYTE == 'X' || INBYTE == 'x'){
+      Serial.println("Movendo para posicao neutra");
+      ajustarPosicaoNeutra();
+    }
   }
 }
+
 void verificarDistancia() {
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
@@ -122,7 +66,7 @@ void verificarDistancia() {
 void moverParaFrente() {
   if (distance > distanciaSegura) {
     digitalWrite(motor_frente, HIGH);
-    delay(600);
+    delay(500);
     digitalWrite(motor_frente, LOW);
   } else {
     pararMovimento();
@@ -130,46 +74,48 @@ void moverParaFrente() {
 }
 
 void moverParaTras() {
-  if (digitalRead(infra_vermelho1) == HIGH){
-    pararMovimento();
+  if (digitalRead(infra_vermelho) == HIGH){
+    digitalWrite(motor_re, LOW);
   } else{
-  digitalWrite(motor_re, HIGH);
-  delay(600);
-  digitalWrite(motor_re, LOW);
+    digitalWrite(motor_re, HIGH);
+    delay(500);
+    digitalWrite(motor_re, LOW);
   }
 }
 
 void virarEsquerda() {
-  for (i = 90; i < 180; i++) {
+  for (i; i < 180; i++) {
     servo.write(i);
-    delay(8);
+    delay(10);
   }
 }
 
 void virarDireita() {
-  for (i = 90; i > 0; i--) {
+  for (i; i > 0; i--) {
     servo.write(i);
-    delay(8);
+    delay(10);
   }
 }
 
 void ajustarPosicaoNeutra() {
-  if(i > 90){
-    for (i = 90; i < 180; i++) {
+  if(i < 90){
+    for (i; i < 90; i++) {
       servo.write(i);
-      delay(8);
+      delay(10);
     }
-  } else {
-    for (i = 180; i > 90; i--) {
+  } else if(i>90){
+    for (i; i > 90; i--) {
       servo.write(i);
-      delay(8);
+      delay(10);
     }
+  }else{
+      servo.write(i);
+      delay(5);
   }
 }
 
 void pararMovimento() {
   // Função para parar todos os movimentos
   digitalWrite(motor_frente, LOW);
-  digitalWrite(motor_re, LOW);
   Serial.println("Obstáculo detectado! Movimento interrompido.");
 }
